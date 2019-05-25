f084f731ea930561cd5fb9a684d69750c05c82dc
Luciano <lucianoluzzi@hotmail.com>
2019-05-25 20:21:20 -0300

    refactoring

--- app/src/main/java/br/com/lucianoluzzi/currencyrate/model/BaseViewModel.kt
+++ app/src/main/java/br/com/lucianoluzzi/currencyrate/model/BaseViewModel.kt
@@ -1,10 +1,10 @@
-package model
+package br.com.lucianoluzzi.currencyrate.model
 
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
-abstract  class BaseViewModel : ViewModel() {
+abstract class BaseViewModel : ViewModel() {
     private val compositeDisposable = CompositeDisposable()
 
     fun addDisposable(disposable: Disposable) {
--- app/src/main/java/br/com/lucianoluzzi/currencyrate/model/RatesViewModel.kt
+++ app/src/main/java/br/com/lucianoluzzi/currencyrate/model/RatesViewModel.kt
@@ -9,7 +9,6 @@ import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
-import model.BaseViewModel
 import java.util.concurrent.TimeUnit
 
 class RatesViewModel(private val rateRepository: RateRepository) : BaseViewModel() {
